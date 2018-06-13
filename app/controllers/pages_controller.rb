6dc0e8e7050693c7e8dd1838f4d47cddd17c4342
YUMEE <yumeehk@hotmail.com>
2018-06-13 15:43:08 -0400

    kitten page works

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,26 @@
 class PagesController < ApplicationController
   def welcome
+
   end
 
   def about
+
   end
 
   def contest
+  end
+
+  def kitten
+    set_kitten_url
+  end
+
+  def kittens
+    set_kitten_url
+  end 
+
+  def set_kitten_url
+    requested_size = params[:size]
+    @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
   end
 
 end
